f50ae08b52460745379310895196cf7675e920ce
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-27 13:00:10 +0530

    Basic Design Done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='myInfo'>
+          <div>
+            <h1>Nitish Mittal</h1>
+            <p>Full Stack Developer</p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
