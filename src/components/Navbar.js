5946dc6d01cc2f724b2cd57748248c3be8cfad0f
nitish mittal <nitish.mittal@orchids.edu.in>
2020-02-01 19:55:43 +0530

    React Modal Configured

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,75 @@
 import React from 'react';
+import ReactModal from "react-modal";
 
-function Navbar(props) {
-    return(
+
+
+class Navbar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showModal: false,
+      modalType: ''
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.renderContent = this.renderContent.bind(this);
+  }
+  handleOpenModal(e) {
+    this.setState({ 
+        showModal: true,
+        modalType: e.target.id
+    });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
+  renderContent() {
+    if(this.state.modalType === 'about') {
+        return <div>About</div>;
+    }
+    if (this.state.modalType === "project") {
+    return <div>Project</div>;
+    }
+    if (this.state.modalType === "contact") {
+    return <div>Contact</div>;
+    }
+    return (
+        <div></div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
         <nav>
-            <ul className="nav">
-                <li>About Me</li>
-                <li>Projects</li>
-                <li>Contact Me</li>
-            </ul>
+          <ul className="nav">
+            <li onClick={this.handleOpenModal} id="about">
+              About Me
+            </li>
+            <li onClick={this.handleOpenModal} id="project">
+              Projects
+            </li>
+            <li onClick={this.handleOpenModal} id="contact">
+              Contact Me
+            </li>
+          </ul>
         </nav>
-    )
+        <ReactModal
+          shouldCloseOnEsc
+          isOpen={this.state.showModal}
+          appElement={document.getElementById("root")}
+          onRequestClose={this.handleCloseModal}
+          contentLabel="About Us Modal"
+          shouldCloseOnOverlayClick={false}
+        >
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+          {this.renderContent()}
+        </ReactModal>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
