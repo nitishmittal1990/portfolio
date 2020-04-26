536ec8c6c2d8bd335e6613c3e18a2f399cb8a20e
Nitish Mittal <nitishmittal1990@gmail.com>
2020-04-26 19:23:14 +0530

    cypress removed

--- cypress/integration/sample_spec.js
+++
@@ -1,7 +0,0 @@
-describe("My First Test", function() {
-  it("Does not do much!", function() {
-      cy.visit('https://example.cypress.io');
-      cy.contains('type').click();
-      cy.get(":nth-child(3) > ul > :nth-child(5) > a").click();
-  });
-});
